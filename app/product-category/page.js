da7591f03bd35eaebf14089c387171c83dd1f684
pooja panwar <poojapanwar@beeonline.co.in>
2025-10-15 17:18:00 +0530

    start

---
+++ app/product-category/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoryHeader from '../../components/ProductCategories/CategoryHeader'
+
+const page = () => {
+  return (
+    <>
+     <CategoryHeader /> 
+    </>
+  )
+}
+
+export default page
---
+++ components/ProductCategories/CategoryHeader.js
@@ -0,0 +1,44 @@
+"use client";
+import React, { useState } from "react";
+import styles from "../../styles/CategoryHeader.module.scss";
+import category1 from "../../public/images/category1.svg";
+import category2 from "../../public/images/category2.svg";
+import category3 from "../../public/images/category3.svg";
+import Image from "next/image";
+
+const CategoryHeader = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const categoryData = [
+    {
+      icon: category1,
+      name: "Components",
+    },
+    {
+      icon: category2,
+      name: "Equipment",
+    },
+    {
+      icon: category3,
+      name: "Materials",
+    },
+  ];
+  return (
+    <div className={styles["category-header-section"]}>
+      <div className={styles["all-category-headings"]}>
+        {categoryData.map((item, index) => (
+          <div
+            className={`${styles["icon-and-text"]} ${
+              activeIndex === index ? styles.active : ""
+            }`}
+            onClick={() => setActiveIndex(index)}
+          >
+            <Image src={item.icon} alt="svg" />
+            <h5>{item.name}</h5>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryHeader;
