e3e36ec6398058686e017ad631348790f511e33e
pooja panwar <poojapanwar@beeonline.co.in>
2025-10-16 14:31:24 +0530

    category-product

--- components/ProductCategories/CategoryHeader.js
+++ components/ProductCategories/CategoryHeader.js
@@ -5,6 +5,7 @@ import category1 from "../../public/images/category1.svg";
 import category2 from "../../public/images/category2.svg";
 import category3 from "../../public/images/category3.svg";
 import Image from "next/image";
+import CategoryProduct from "./CategoryProduct";
 
 const CategoryHeader = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -23,22 +24,25 @@ const CategoryHeader = () => {
     },
   ];
   return (
-    <div className={styles["category-header-section"]}>
-      <div className={styles["all-category-headings"]}>
-        {categoryData.map((item, index) => (
-          <div
-            key={index}
-            className={`${styles["icon-and-text"]} ${
-              activeIndex === index ? styles.active : ""
-            }`}
-            onClick={() => setActiveIndex(index)}
-          >
-            <Image src={item.icon} alt="svg" />
-            <h5>{item.name}</h5>
-          </div>
-        ))}
+    <>
+      <div className={styles["category-header-section"]}>
+        <div className={styles["all-category-headings"]}>
+          {categoryData.map((item, index) => (
+            <div
+              key={index}
+              className={`${styles["icon-and-text"]} ${
+                activeIndex === index ? styles.active : ""
+              }`}
+              onClick={() => setActiveIndex(index)}
+            >
+              <Image src={item.icon} alt="svg" />
+              <h5>{item.name}</h5>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <CategoryProduct />
+    </>
   );
 };
 
---
+++ components/ProductCategories/CategoryProduct.js
@@ -0,0 +1,112 @@
+import React from "react";
+import styles from "../../styles/CategoryProduct.module.scss";
+import product1 from "../../public/images/product1.png";
+import product2 from "../../public/images/product2.png";
+import product3 from "../../public/images/product3.png";
+import Image from "next/image";
+const CategoryProduct = () => {
+  const cardsData = [
+    {
+      image: product1,
+      name: "SR300Photo Voltaic Float Zone Silicon wafer0",
+      details:
+        "Photo Voltaic Float Zone(PV-FZ) silicon wafers have High minority carrier ...",
+    },
+    {
+      image: product2,
+      name: "Preferred Float Zone Silicon Wafers",
+      details:
+        "Float Zone-Preferred Float Zone(FZ-PFZ) silicon wafer has the lowest resist..",
+    },
+    {
+      image: product3,
+      name: "Float Zone Silicon Wafers",
+      details:
+        "Most power electronics devices are manufactured using float zone wafers for...",
+    },
+    {
+      image: product1,
+      name: "SR300Photo Voltaic Float Zone Silicon wafer0",
+      details:
+        "Photo Voltaic Float Zone(PV-FZ) silicon wafers have High minority carrier ...",
+    },
+    {
+      image: product1,
+      name: "SR300Photo Voltaic Float Zone Silicon wafer0",
+      details:
+        "Photo Voltaic Float Zone(PV-FZ) silicon wafers have High minority carrier ...",
+    },
+    {
+      image: product2,
+      name: "Preferred Float Zone Silicon Wafers",
+      details:
+        "Float Zone-Preferred Float Zone(FZ-PFZ) silicon wafer has the lowest resist..",
+    },
+    {
+      image: product3,
+      name: "Float Zone Silicon Wafers",
+      details:
+        "Most power electronics devices are manufactured using float zone wafers for...",
+    },
+    {
+      image: product1,
+      name: "SR300Photo Voltaic Float Zone Silicon wafer0",
+      details:
+        "Photo Voltaic Float Zone(PV-FZ) silicon wafers have High minority carrier ...",
+    },
+    {
+      image: product1,
+      name: "SR300Photo Voltaic Float Zone Silicon wafer0",
+      details:
+        "Photo Voltaic Float Zone(PV-FZ) silicon wafers have High minority carrier ...",
+    },
+    {
+      image: product2,
+      name: "Preferred Float Zone Silicon Wafers",
+      details:
+        "Float Zone-Preferred Float Zone(FZ-PFZ) silicon wafer has the lowest resist..",
+    },
+    {
+      image: product3,
+      name: "Float Zone Silicon Wafers",
+      details:
+        "Most power electronics devices are manufactured using float zone wafers for...",
+    },
+    {
+      image: product1,
+      name: "SR300Photo Voltaic Float Zone Silicon wafer0",
+      details:
+        "Photo Voltaic Float Zone(PV-FZ) silicon wafers have High minority carrier ...",
+    },
+  ];
+  return (
+    <div className={styles["all-category-product-section"]}>
+      <div className={styles["category-text-and-cards"]}>
+        <h2>
+          Equipment <span>48 products found</span>
+        </h2>
+        <p>
+          Range of Equipment are mainly to semiconductors and microelectronics
+          fabrication and assembly
+        </p>
+        <div className={styles["category-main-cards-sections"]}>
+            {
+                cardsData.map((item,index)=>(
+                    <div className={styles["product-card"]} key={index}>
+                       <div className={styles["product-image"]}>
+                        <Image src={item.image} alt="image"/>
+                       </div>
+                       <h5>{item.name}</h5>
+                       <p>{item.details}</p>
+                       <button className={styles["view-product-btn"]}>View Product</button>
+                       <button className={styles["add-cart-btn"]}>Add to Cart</button>
+                    </div>
+                ))
+            }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryProduct;
